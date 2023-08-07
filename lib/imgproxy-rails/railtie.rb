e213c1efc45aa1812da8bf2ecfc4cb5c6e3a3613
Albert <ext-albert.padzerin@sbermarket.ru>
2023-08-07 19:17:26 +0400

    Use #direct method to initialize url helpers

--- lib/imgproxy-rails/railtie.rb
+++ lib/imgproxy-rails/railtie.rb
@@ -4,38 +4,26 @@ require "imgproxy"
 require "imgproxy-rails/transformer"
 
 module ImgproxyRails
-  module UrlHelpers
+  class Railtie < ::Rails::Railtie
     VARIANT_CLASSES = [
       "ActiveStorage::Variant",
       "ActiveStorage::VariantWithRecord"
     ].freeze
 
-    def imgproxy_active_storage_url(*args)
-      record = args[0]
-
-      if VARIANT_CLASSES.any? { |klass| record.is_a?(klass.constantize) }
-        imgproxy_url(record)
-      else
-        rails_storage_proxy_url(*args)
+    initializer "imgproxy-rails.routes" do
+      config.after_initialize do |app|
+        app.routes.prepend do
+          direct :imgproxy_active_storage do |model, options|
+            if VARIANT_CLASSES.any? { |klass| model.is_a?(klass.constantize) }
+              url = rails_storage_proxy_url(model.blob)
+              transformations = model.variation.transformations
+              ::Imgproxy.url_for(url, Transformer.call(transformations))
+            else
+              rails_storage_proxy_url(model, options)
+            end
+          end
+        end
       end
-    end
-
-    def imgproxy_active_storage_path(*args)
-      rails_storage_proxy_path(*args)
-    end
-
-    private
-
-    def imgproxy_url(record)
-      transformations = record.variation.transformations
-      url = rails_storage_proxy_url(record.blob)
-      ::Imgproxy.url_for(url, Transformer.call(transformations))
-    end
-  end
-
-  class RailtieHelpers < ::Rails::Railtie
-    initializer "imgproxy-rails.include_url_helpers" do
-      ActionDispatch::Routing::UrlFor.include UrlHelpers
     end
   end
 end
--- spec/internal_spec.rb
+++ spec/internal_spec.rb
@@ -1,8 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "Dummy app", type: :request do
-  include ActionView::Helpers::AssetTagHelper
-
   let(:option) { record }
   let(:record) { user.avatar.variant(resize_to_limit: [500, 500]) }
   let(:user) do
@@ -15,9 +13,14 @@ RSpec.describe "Dummy app", type: :request do
     end
   end
 
-  before { Rails.application.routes.default_url_options[:host] = "http://example.com" }
+  before do
+    Rails.application.routes.default_url_options[:host] = "http://example.com"
+    Imgproxy.configure { |config| config.endpoint = "http://imgproxy.io" }
+  end
 
   describe "image_tag" do
+    include ActionView::Helpers::AssetTagHelper
+
     subject { image_tag(option) }
 
     shared_context "handles string correctly" do
@@ -34,12 +37,7 @@ RSpec.describe "Dummy app", type: :request do
     end
 
     describe "with resolve_model_to_route = :imgproxy_active_storage" do
-      before do
-        ActiveStorage.resolve_model_to_route = :imgproxy_active_storage
-        Imgproxy.configure do |config|
-          config.endpoint = "http://imgproxy.io"
-        end
-      end
+      before { ActiveStorage.resolve_model_to_route = :imgproxy_active_storage }
 
       it { is_expected.to include('<img src="http://imgproxy.io', "blobs") }
 
@@ -64,20 +62,16 @@ RSpec.describe "Dummy app", type: :request do
   end
 
   describe "url helpers" do
+    describe "#imgproxy_active_storage_url" do
+      subject { imgproxy_active_storage_url(option) }
+
+      it { is_expected.to start_with("http://imgproxy.io/unsafe").and include('rails/active_storage') }
+    end
+
     describe "#imgproxy_active_storage_path" do
       subject { imgproxy_active_storage_path(option) }
 
-      describe "with resolve_model_to_route == :imgproxy_active_storage" do
-        before { ActiveStorage.resolve_model_to_route = :imgproxy_active_storage }
-
-        it { is_expected.to start_with("/rails/active_storage") }
-      end
-
-      describe "with resolve_model_to_route == :rails_storage_proxy" do
-        before { ActiveStorage.resolve_model_to_route = :rails_storage_proxy }
-
-        it { is_expected.to start_with("/rails/active_storage") }
-      end
+      it { is_expected.to start_with("/unsafe").and include('rails/active_storage') }
     end
   end
 end
