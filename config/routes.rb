8001f77e4b4ce6a7f238ddf4e44fdc0be6605a9d
Albert <ext-albert.padzerin@sbermarket.ru>
2023-08-12 03:02:28 +0400

    Move variation check to helper

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   direct :imgproxy_active_storage do |model, options|
-    if model.respond_to?(:variation) && model.try(:blob)&.content_type&.split("/")&.first == "image"
+    if ImgproxyRails::Helpers.image_variation?(model)
       url = route_for(:rails_storage_proxy, model.blob, options)
       transformations = model.variation.transformations
       Imgproxy.url_for(url, ImgproxyRails::Transformer.call(transformations, model.blob.metadata))
--- lib/imgproxy-rails.rb
+++ lib/imgproxy-rails.rb
@@ -4,4 +4,5 @@ require "imgproxy"
 
 require "imgproxy-rails/engine"
 require "imgproxy-rails/transformer"
+require "imgproxy-rails/helpers"
 require "imgproxy-rails/version"
